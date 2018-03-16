295f9cd8f30ae31facfd78b66a36a43d27856e27
TheMichaelB <michael@azured.io>
2018-03-16 14:41:05 -0700

    added log entries loging

--- files/scripts/azurelogin.sh
+++ files/scripts/azurelogin.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
 # Script Name: azurelogin.sh
 # Author: Michael Boswell - azured.io - github(themichaelb)
-# Version: 0.1.1
+# Version: 0.1
 # Description:
 # This script uses azure cli application to log into Azure using a service principal via a certificate
 # Once logged in the script will switch to the subscription specified. 
@@ -12,6 +11,11 @@
 #  3 - s  Subscription name to log in to
 #  4 - u  Unique service principal identity to log in with 
 #  5 - h  Help
+
+
+verbosity=2 #Start counting at 2 so that any increase to this will result in a minimum of file descriptor 3.  You should leave this alone.
+maxverbosity=7 #The highest verbosity we use / allow to be displayed.  Feel free to adjust.
+
 
 
 help()
@@ -26,7 +30,7 @@ help()
     echo "   -h         this help message"
 }
 
-while getopts c:t:u:s:h optname; do
+while getopts c:t:u:s:h:v optname; do
   case ${optname} in
     c)  #certificate path 
       CERT_PATH=${OPTARG}
@@ -40,6 +44,9 @@ while getopts c:t:u:s:h optname; do
     s) #Subscription name to log into 
       SUBSCRIPTION_NAME=${OPTARG}
       ;;
+    v) # increase verbosity
+      (( verbosity=verbosity+1 ))
+      ;;
     h)  #show help
       help
       exit 2
@@ -51,32 +58,58 @@ while getopts c:t:u:s:h optname; do
       ;;
   esac
 done
+echo "Verbosity = $verbosity"
+# stolen from - https://stackoverflow.com/a/33597663/5255018
+declare -A LOG_LEVELS
+LOG_LEVELS=([0]="emerg" [1]="alert" [2]="crit" [3]="err" [4]="warning" [5]="notice" [6]="info" [7]="debug")
+function .log () {
+  local LEVEL=${1}
+  shift
+  if [ ${verbosity} -ge ${LEVEL} ]; then
+    LOG=$(echo "{\"LOGLEVEL\":\"${LOG_LEVELS[$LEVEL]}\",\"$1\":$2}" | jq -c .)
+    curl -H "Content-Type: application/json" -X POST -d "$LOG"  http://webhook.logentries.com/noformat/logs/$(cat /secrets/logentries/TwoEightyCap.Deploy.key)
+    echo $LOG
+  fi
+}
 
 
 if [ -z "$CERT_PATH" ]; then
+  .log 1 "CERT_PATH not set" "\"CERT_PATH not set\"" 
   echo "No certificate specified"
   exit 1 # error
+else 
+.log 7 "CERT_PATH SET TO" "\"$CERT_PATH"\"
 fi 
 
 
 if [ -z "$TENANT_NAME" ]; then
+  .log 1 "TENANT_NAME not set" "\"TENANT_NAME not set\"" 
   echo "No tenant specified"
   exit 1 # error
+else 
+.log 7 "TENANT_NAME SET TO" "\"$TENANT_NAME\""
 fi 
 
 
 if [ -z "$PRINCIPLE_ID" ]; then
+  .log 1 "PRINCIPLE_ID not set" "\"PRINCIPLE_ID not set\"" 
   echo "No Service Principal Id specified"
   exit 1 # error
+else 
+.log 7 "PRINCIPLE_ID SET TO" "\"$PRINCIPLE_ID\""
 fi 
 
 
 if [ -z "$SUBSCRIPTION_NAME" ]; then
+  .log 1 "SUBSCRIPTION_NAME not set" "\"SUBSCRIPTION_NAME not set\"" 
   echo "No Subscription specified"
   exit 1 # error
+else 
+.log 7 "SUBSCRIPTION_NAME SET TO" "\"$SUBSCRIPTION_NAME\""
 fi
 
 if [ ! -f /tmpfs/$CertificateName ]; then
+    .log 7 "decoding certificate" "\"$CertificateName\""
     echo "decoding certificate"
     mkdir -p /certs
     base64 -d /secrets/$CertificateName.gpg.base64 > /certs/$CertificateName.gpg 
@@ -88,10 +121,10 @@ CERT_PATH=/tmpfs/$CertificateName
 ACCOUNTS=$(az login --service-principal -u $PRINCIPLE_ID \
     -p $CERT_PATH \
     --tenant $TENANT_NAME)
-
-
+    
+.log 6 login $(echo $ACCOUNTS | jq -c .)
 ACCOUNT=$(az account set -s $SUBSCRIPTION_NAME)
-echo $ACCOUNT 
+
  
 
 CURRENT_CONTEXT=$(az account show)
@@ -101,7 +134,10 @@ LC_SUBSCRIPTION_NAME=$(echo ${SUBSCRIPTION_NAME,,})
 
 if [ "$LC_CONTEXT_TEST" == "$LC_SUBSCRIPTION_NAME" ]
 then
+
+.log 6 "Successful Login" $(echo $CURRENT_CONTEXT | jq -c .)
 exit 0
 else
+.log 1 "Unsuccessful Login" $(echo $CURRENT_CONTEXT | jq -c .)
 exit 2
 fi
--- files/scripts/new-resourcegroup.sh
+++ files/scripts/new-resourcegroup.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-
-/scripts/azurelogin.sh -c $CertificateName -t $TenantId -u $ServicePrincipleIdentity -s $Subscription 
+# stolen from - https://stackoverflow.com/a/33597663/5255018
+verbosity=7
+declare -A LOG_LEVELS
+LOG_LEVELS=([0]="emerg" [1]="alert" [2]="crit" [3]="err" [4]="warning" [5]="notice" [6]="info" [7]="debug")
+function .log () {
+  local LEVEL=${1}
+  shift
+  if [ ${verbosity} -ge ${LEVEL} ]; then
+    LOG=$(echo "{\"LOGLEVEL\":\"${LOG_LEVELS[$LEVEL]}\",\"$1\":$2}" | jq -c .)
+    curl -H "Content-Type: application/json" -X POST -d "$LOG"  http://webhook.logentries.com/noformat/logs/$(cat /secrets/logentries/TwoEightyCap.Deploy.key)
+    echo $LOG
+  fi
+}
+/scripts/azurelogin.sh -c $CertificateName -t $TenantId -u $ServicePrincipleIdentity -s $Subscription -vvvvv
 
 if [ $? -eq 0 ]; then
 RANDOM=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
 RG="Testrg-$RANDOM"
-az group create --name $RG --location northeurope 
+RESOURCEGROUP=$(az group create --name $RG --location northeurope )
+
+.log 6 "Successfully Created resource group" $(echo $RESOURCEGROUP | jq -c .)
+else 
+
+.log 1 "Unsuccessful Login from new-resourcegroup" $(echo $CURRENT_CONTEXT | jq -c .)
 fi 
